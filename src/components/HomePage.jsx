2b29b7d7fdd69991d58b481f1fda176c0c35b770
ukoji8888 <leekashing1996@gmail.com>
2020-09-22 12:07:19 +0800

    Add Router

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return <h1>Hello World!</h1>;
+    }
+}
+
+class SubPage1 extends Component {
+    render() {
+        return <h1>SubPage1</h1>
+    }
+}
+
+export { HomePage, SubPage1 };
